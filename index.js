4305e698a38f8c3cac96c71a4e849b1d3080d698
Saim Rizvi <shr2mz@gmail.com>
2021-10-03 17:59:58 -0400

    commit tree 40d5ba8d6abb874d3d24d1eee3650c40e7836097

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  
+  for (let i=0; i < word.length / 2; i++) {
+    const endIndex = word.length - 1 - i;
+    if (word[i] !== word[endIndex]) {
+    return false
+    } else {
+    return true 
+    }
+  
+  }
 }
 
 /* 
   Add your pseudocode here
+  function isPalidrome (){
+    a, b, c
+    c, b, a
+  }
+
+
 */
 
 /*
   Add written explanation of your solution here
+
+  if the word is palidrome ex. mom 
+  then return TRUE 
+  if it is NOT palidrome 
+  then return FALSE 
 */
 
 // You can run `node index.js` to view these console logs
